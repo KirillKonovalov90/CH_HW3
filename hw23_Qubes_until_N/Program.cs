c1ae210b864a3529067564e4bf52086aefcabb7b
Kirill <wakened90@gmail.com>
2022-12-26 18:52:44 +0300

    qubes works

---
+++ hw23_Qubes_until_N/Program.cs
@@ -0,0 +1,19 @@
+﻿void Qubes_to_N(int N)
+{
+    int count;
+
+    count = 1;
+    
+    while(count <= N)
+    {
+        Console.Write($"|  {count*count*count}  |");
+        count++;
+    }
+}
+
+int numN;
+
+Console.Write("Введите число N: ");
+numN = int.Parse(Console.ReadLine()!);
+
+Qubes_to_N(numN);
