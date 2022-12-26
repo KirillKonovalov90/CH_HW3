d1f590048effe0e09f39ca0bf4b7707a50982d6e
Kirill <wakened90@gmail.com>
2022-12-26 18:58:46 +0300

    some minor changes final

---
+++ hw23_QubesUntilN/Program.cs
@@ -0,0 +1,19 @@
+﻿void QubesToN(int N)
+{
+    int count;
+
+    count = 1;
+    
+    while(count <= N)
+    {
+        Console.Write($"|  {count*count*count}  |");
+        count++;
+    }
+}
+
+int numN;
+
+Console.Write("Введите число N: ");
+numN = int.Parse(Console.ReadLine()!);
+
+QubesToN(numN);
