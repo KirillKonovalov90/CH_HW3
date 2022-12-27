e0287387113e5e608e0ae118e954a362c904e090
Kirill <wakened90@gmail.com>
2022-12-27 11:46:48 +0300

    fixes with help from tutor

--- hw21_DistanceIn3D/Program.cs
+++ hw21_DistanceIn3D/Program.cs
@@ -1,11 +1,9 @@
 ﻿                                                  //Программа для вычисления расстояния между 2мя точками в трехмерном пространстве
                                                   //Использовал подсказанный Вами метод ввода, очень удобно, спасибо :)
 
-double DistanceBetweenDots(int dotA0, int dotA1, int dotA2, int dotB0, int dotB1, int dotB2)
+double DistanceBetweenDots(int[] A, int[] B)
  {
      double AB;
-     int[] A = new int[] {dotA0, dotA1, dotA2};
-     int[] B = new int[] {dotB0, dotB1, dotB2};
      AB = Math.Sqrt(((A[0] - B[0]) * (A[0] - B[0])) + ((A[1] - B[1]) * (A[1] - B[1])) + ((A[2] - B[2]) * (A[2] - B[2])));
      return AB;
  }
@@ -16,4 +14,4 @@ var dotA = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
 Console.Write("Введите координаты точки B через пробел: ");
 var dotB = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
 
-Console.WriteLine(DistanceBetweenDots(dotA[0], dotA[1], dotA[2], dotB[0], dotB[1], dotB[2]));
+Console.WriteLine(DistanceBetweenDots(dotA, dotB));
